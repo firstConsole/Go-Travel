26a316b961a3da1ee9742821ab9accc829841db8
Артамонов Алексей Викторович <94192633+firstConsole@users.noreply.github.com>
2023-05-21 15:03:09 +0300

    Initial Commit

---
+++ Go Travel/Go_TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Go_TravelApp.swift
+//  Go Travel
+//
+//  Created by Алексей Артамонов on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Go_TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
